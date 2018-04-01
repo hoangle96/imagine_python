c8911b3380661106740a2215c7a4c5ff451eb53d
hoangle96 <lebahoang010296@gmail.com>
2018-04-01 13:24:48 -0400

    adding symbol table

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ def parseToken(s):
     return tokens
 
 def isOperator(s):
-    return s in ["+", "-", "/", "*", "^", "%"]
+    return s in ["+", "-", "/", "*", "^", "%", "="]
 
-def isReversed(s):
+def isReserved(s):
     return s in ["get", "output", "goto", "stop", "let", "if"]
 
 tokens = []
@@ -19,6 +19,37 @@ with open('test.txt', "r") as f:
         frll = frl.replace("\n", "").split(" ")
         for i in frll:
             if i == "cmt":
+                tokens.append(i)
                 break
             tokens.append(i)
 print(tokens)
+
+min = 0
+max = 99
+
+table = []
+with open('test.txt', "r") as f:
+    for frl in f.readlines():
+        frll = frl.replace("\n", "").split(" ")
+        table.append(["L",frll[0], min])
+        min += 1
+        for i in frll:
+            if i == "cmt":
+                break
+            elif not(isOperator(i)) or not(isReserved(i)):
+                print(isReserved(i))
+                print(i)
+                # if i.isdigit():
+                #     table.append(["C", i, max])
+                #     max =- 1
+                #     #print(not isReserved(i))
+                #     #print(i)
+                # elif i.isalpha() or i.isalnum():
+                #     table.append(["V", i, max])
+                #     max =- 1
+                #     # print(not isReserved(i))
+                #     # print(i)
+# print(table)
+
+#print(not isReserved("goto"))
+
