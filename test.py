c50ad82c4287aeeb3a5dd5f0d148e92a0152cf60
hoangle96 <lebahoang010296@gmail.com>
2018-04-01 16:07:53 -0400

    Final project to test

--- test.py
+++ test.py
@@ -7,11 +7,14 @@ def parseToken(s):
         tokens.append(i)
     return tokens
 
+
 def isOperator(s):
     return s in ["+", "-", "/", "*", "^", "%", "="]
 
+
 def isReserved(s):
     return s in ["get", "output", "goto", "stop", "let", "if"]
+
 
 tokens = []
 with open('test.txt', "r") as f:
@@ -28,27 +31,27 @@ min = 0
 max = 99
 
 table = []
+dup = []
 with open('test.txt', "r") as f:
     for frl in f.readlines():
         frll = frl.replace("\n", "").split(" ")
-        # table.append(["L", frll[0], min])
-        # min += 1
         for i in frll:
-            if frll.index(i) == 0:
-                table.append(["L", frll[0], min])
-                min += 1
-            elif i == "cmt":
-                break
-            elif not(isOperator(i)) and not(isReserved(i)):
-                if i.isdigit():
-                    table.append(["C", i, max])
-                    max -= 1
-                elif i.isalpha() or i.isalnum():
-                    table.append(["V", i, max])
-                    max -= 1
+            if i not in dup:
+                if i == "goto":
+                    break
+                if frll.index(i) == 0:
+                    table.append(["L", frll[0], min])
+                    min += 1
+                elif i == "cmt":
+                    break
+                elif not (isOperator(i)) and not (isReserved(i)):
+                    if i.isdigit():
+                        table.append(["C", i, max])
+                        max -= 1
+                    elif i.isalpha() or i.isalnum():
+                        table.append(["V", i, max])
+                        max -= 1
+                    dup.append(i)
 
 for i in table:
     print(i)
-
-print(isReserved("get"))
-
