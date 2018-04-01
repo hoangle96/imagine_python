af93e68d1160fc28616f5b928ea145c6f88322fb
hoangle96 <lebahoang010296@gmail.com>
2018-04-01 13:40:10 -0400

    symbol table

--- test.py
+++ test.py
@@ -31,25 +31,24 @@ table = []
 with open('test.txt', "r") as f:
     for frl in f.readlines():
         frll = frl.replace("\n", "").split(" ")
-        table.append(["L",frll[0], min])
-        min += 1
+        # table.append(["L", frll[0], min])
+        # min += 1
         for i in frll:
-            if i == "cmt":
+            if frll.index(i) == 0:
+                table.append(["L", frll[0], min])
+                min += 1
+            elif i == "cmt":
                 break
-            elif not(isOperator(i)) or not(isReserved(i)):
-                print(isReserved(i))
-                print(i)
-                # if i.isdigit():
-                #     table.append(["C", i, max])
-                #     max =- 1
-                #     #print(not isReserved(i))
-                #     #print(i)
-                # elif i.isalpha() or i.isalnum():
-                #     table.append(["V", i, max])
-                #     max =- 1
-                #     # print(not isReserved(i))
-                #     # print(i)
-# print(table)
+            elif not(isOperator(i)) and not(isReserved(i)):
+                if i.isdigit():
+                    table.append(["C", i, max])
+                    max -= 1
+                elif i.isalpha() or i.isalnum():
+                    table.append(["V", i, max])
+                    max -= 1
 
-#print(not isReserved("goto"))
+for i in table:
+    print(i)
+
+print(isReserved("get"))
 
